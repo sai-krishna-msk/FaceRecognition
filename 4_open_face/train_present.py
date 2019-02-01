519096a39812004287d9d6907a2eec1a7a2a46cb
Sai Krishna <saimanthena13579@gmail.com>
2019-02-01 19:58:18 +0530

    Update train_present.py

--- 4_open_face/train_present.py
+++ 4_open_face/train_present.py
@@ -1,13 +1,13 @@
-<<<<<<< HEAD
+
 from helper import embeddingsGenrerator
 from helper import trainSVM
 
 embeddingsGenrerator()
 trainSVM()
-=======
+
 from helper import embeddingsGenrerator
 from helper import trainSVM
 
 embeddingsGenrerator()
 trainSVM()
->>>>>>> 292c2e4c5ed8938196724f06c57ad94d8f8f5d92
+
