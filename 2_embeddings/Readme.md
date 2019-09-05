5d73668fa310191f7bd65e0922510e1710e794a0
sai_krishna <saimanthena13579@gmail.com>
2019-09-05 10:39:55 +0530

    Cleaned 2 image producing embeddings

--- 2_embeddings/Readme.md
+++ 2_embeddings/Readme.md
@@ -1,4 +1,3 @@
-
 For implementing-:
 
 
@@ -14,33 +13,9 @@ conda install -c menpo dlib
 pip install imutils
 ```
 
-At present I have seeded the dataset folder with elon musk , leonardo decaprio and steve jobs images , You can fill in your photos and test that out 
+At present I have seeded the dataset folder with Elon musk , Leonardo DiCaprio and steve jobs images , You can fill in your photos and test that out 
 
-Here we are producing embeddings, You can use them to, train your own model(try experimenting)
-```
-python test.py
-```
-You can find your embeddings in output folder after running the model
-During the process of training you can view which part of the image (face) is being converted to embeddings
-=======
-For implementing-:
-
-
-
-
-Installing the required dependencies-:
-> Open the command prompt
-> navigate to folder (inside the folder)
-> enter-:
-```
-pip install opencv-contrib-python
-conda install -c menpo dlib
-pip install imutils
-```
-
-At present I have seeded the dataset folder with elon musk , leonardo decaprio and steve jobs images , You can fill in your photos and test that out 
-
-Here we are producing embeddings, You can use them to, train your own model(try experimenting)
+Here we are producing embeddings, You can use them to train your own model(try experimenting)
 ```
 python test.py
 ```
--- 2_embeddings/train.py
+++ 2_embeddings/train.py
@@ -16,6 +16,7 @@ embedder = cv2.dnn.readNetFromTorch("model/openface_nn4.small2.v1.t7")
 
 imagePaths = list(paths.list_images("dataset"))
 
+print(imagePaths)
 # initialize our lists of extracted facial embeddings and
 # corresponding people names
 knownEmbeddings = []
@@ -36,60 +37,62 @@ for (i, imagePath) in enumerate(imagePaths):
 	# dimensions
 	image = cv2.imread(imagePath)
 	if(image is None):
-		break
-	image = imutils.resize(image, width=600)
-	(h, w) = image.shape[:2]
+		pass
+	else:
+		
+		image = imutils.resize(image, width=600)
+		(h, w) = image.shape[:2]
 
-	# construct a blob from the image
-	imageBlob = cv2.dnn.blobFromImage(
-		cv2.resize(image, (300, 300)), 1.0, (300, 300),
-		(104.0, 177.0, 123.0), swapRB=False, crop=False)
+		# construct a blob from the image
+		imageBlob = cv2.dnn.blobFromImage(
+			cv2.resize(image, (300, 300)), 1.0, (300, 300),
+			(104.0, 177.0, 123.0), swapRB=False, crop=False)
 
-	# apply OpenCV's deep learning-based face detector to localize
-	# faces in the input image
-	detector.setInput(imageBlob)
-	detections = detector.forward()
+		# apply OpenCV's deep learning-based face detector to localize
+		# faces in the input image
+		detector.setInput(imageBlob)
+		detections = detector.forward()
 
-	# ensure at least one face was found
-	if len(detections) > 0:
-		# we're making the assumption that each image has only ONE
-		# face, so find the bounding box with the largest probability
-		i = np.argmax(detections[0, 0, :, 2])
-		confidence = detections[0, 0, i, 2]
+		# ensure at least one face was found
+		if len(detections) > 0:
+			# we're making the assumption that each image has only ONE
+			# face, so find the bounding box with the largest probability
+			i = np.argmax(detections[0, 0, :, 2])
+			confidence = detections[0, 0, i, 2]
 
-		# ensure that the detection with the largest probability also
-		# means our minimum probability test (thus helping filter out
-		# weak detections)
-		if confidence > 0.95:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the face
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
+			# ensure that the detection with the largest probability also
+			# means our minimum probability test (thus helping filter out
+			# weak detections)
+			if confidence > 0.95:
+				# compute the (x, y)-coordinates of the bounding box for
+				# the face
+				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+				(startX, startY, endX, endY) = box.astype("int")
 
-			# extract the face ROI and grab the ROI dimensions
-			face = image[startY:endY, startX:endX]
-			cv2.imshow("faces" , face)
-			if(cv2.waitKey(1) and 0xFF==27):
-				break
-			(fH, fW) = face.shape[:2]
+				# extract the face ROI and grab the ROI dimensions
+				face = image[startY:endY, startX:endX]
+				cv2.imshow("faces" , face)
+				if(cv2.waitKey(1) and 0xFF==27):
+					break
+				(fH, fW) = face.shape[:2]
 
-			# ensure the face width and height are sufficiently large
-			if fW < 20 or fH < 20:
-				continue
+				# ensure the face width and height are sufficiently large
+				if fW < 20 or fH < 20:
+					continue
 
-			# construct a blob for the face ROI, then pass the blob
-			# through our face embedding model to obtain the 128-d
-			# quantification of the face
-			faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
-				(96, 96), (0, 0, 0), swapRB=True, crop=False)
-			embedder.setInput(faceBlob)
-			vec = embedder.forward()
+				# construct a blob for the face ROI, then pass the blob
+				# through our face embedding model to obtain the 128-d
+				# quantification of the face
+				faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
+					(96, 96), (0, 0, 0), swapRB=True, crop=False)
+				embedder.setInput(faceBlob)
+				vec = embedder.forward()
 
-			# add the name of the person + corresponding face
-			# embedding to their respective lists
-			knownNames.append(name)
-			knownEmbeddings.append(vec.flatten())
-			total += 1
+				# add the name of the person + corresponding face
+				# embedding to their respective lists
+				knownNames.append(name)
+				knownEmbeddings.append(vec.flatten())
+				total += 1
 
 print("[INFO] serializing {} encodings...".format(total))
 data = {"embeddings": knownEmbeddings, "names": knownNames}
